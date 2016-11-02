8088503fb3a51329efc560e75aa4469e5e88b18b
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-11-02 20:59:45 -0200

    create MySQLiteHelper (not fully implemented)

--- app/src/main/java/com/example/rodri/vcatcher/activity/MainActivity.java
+++ app/src/main/java/com/example/rodri/vcatcher/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.rodri.vcatcher;
+package com.example.rodri.vcatcher.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.rodri.vcatcher.R;
 
 public class MainActivity extends AppCompatActivity {
 
