ea2a8e7dd09f4cf3837bbb0c8347e1e63fa6308b
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-11-20 19:24:21 -0200

    implement createGame(), createImage() and createLevel() in MyDataSource

--- app/src/main/java/com/example/rodri/vcatcher/database/MyDataSource.java
+++ app/src/main/java/com/example/rodri/vcatcher/database/MyDataSource.java
@@ -1,8 +1,14 @@
 package com.example.rodri.vcatcher.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.example.rodri.vcatcher.model.Game;
+import com.example.rodri.vcatcher.model.Image;
+import com.example.rodri.vcatcher.model.Level;
 
 /**
  * Created by rodri on 11/5/2016.
@@ -81,5 +87,106 @@ public class MyDataSource {
     }
 
     /** --- CREATE --- */
+
+    public Game createGame(String name) {
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.KEY_NAME, name);
+
+        long insertedId = database.insert(MySQLiteHelper.TABLE_GAME, null, values);
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_GAME, gameColumns,
+                MySQLiteHelper.KEY_ID + " = " + insertedId, null, null, null, null, null);
+
+        if (isCursorEmpty(cursor)) {
+            cursor.close();
+            return null;
+        }
+        cursor.moveToFirst();
+
+        Game newGame = cursorToGame(cursor);
+        cursor.close();
+
+        return newGame;
+
+    }
+
+    public Image createImage(String path, int size) {
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.COLUMN_PATH, path);
+        values.put(MySQLiteHelper.COLUMN_SIZE, size);
+
+        long insertedId = database.insert(MySQLiteHelper.TABLE_IMAGE, null, values);
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_IMAGE, imageColumns,
+                MySQLiteHelper.KEY_ID + " = " + insertedId, null, null, null, null, null);
+
+        if (isCursorEmpty(cursor)) {
+            cursor.close();
+            return null;
+        }
+        cursor.moveToFirst();
+
+        Image newImage = cursorToImage(cursor);
+        cursor.close();
+
+        return newImage;
+
+    }
+
+    public Level createLevel(int num, int experience) {
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.COLUMN_NUM, num);
+        values.put(MySQLiteHelper.COLUMN_EXPERIENCE, experience);
+
+        long insertedId = database.insert(MySQLiteHelper.TABLE_LEVEL, null, values);
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_LEVEL, levelColumns,
+                MySQLiteHelper.KEY_ID + " = " + insertedId, null, null, null, null, null);
+
+        if (isCursorEmpty(cursor)) {
+            cursor.close();
+            return null;
+        }
+        cursor.moveToFirst();
+
+        Level newLevel = cursorToLevel(cursor);
+        cursor.close();
+
+        return newLevel;
+
+    }
+
+    /** --- CURSOR TO --- */
+
+    public Game cursorToGame(Cursor cursor) {
+        Game game = new Game();
+        game.setId(cursor.getLong(0));
+        game.setName(cursor.getString(1));
+        return game;
+    }
+
+    public Image cursorToImage(Cursor cursor) {
+        Image image = new Image();
+        image.setId(cursor.getLong(0));
+        image.setPath(cursor.getString(1));
+        image.setSize(cursor.getInt(2));
+        return image;
+    }
+
+    public Level cursorToLevel(Cursor cursor) {
+        Level level = new Level();
+        level.setId(cursor.getLong(0));
+        level.setNum(cursor.getInt(1));
+        level.setExperience(cursor.getInt(2));
+        return level;
+    }
+
+    /** --- EXTRAS --- */
+
+    public boolean isCursorEmpty(Cursor cursor) {
+        // if the cursor can move to first, than it means it is not empty (Duh!)
+        if (cursor.moveToFirst()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 }
